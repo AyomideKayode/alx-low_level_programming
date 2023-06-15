01ee996334b54dba770e0aa0f9a67e88d6b0a361
Ayomide Kayode <ayomidekay7@gmail.com>
2023-06-15 10:38:59 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 /**
- * main - should print exactly "Programming is like building a multilingual puzzle
- * return:0 program should end with the value 0
+ * main - print "Programming is like building a multilingual puzzle
+ * Return: 0
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
