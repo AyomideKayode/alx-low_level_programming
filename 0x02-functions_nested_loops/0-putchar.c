9bc65ca6a18d5120a9ef906a5daedc7c6f5ca0b3
Ayomide Kayode <ayomidekay7@gmail.com>
2023-06-20 13:44:40 -0700

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -7,10 +7,10 @@
  */
 int main(void)
 {
-	char put[] = "_putchar";
-	int i = 0;
+	char put[10] = "_putchar";
+	int i;
 
-	while (put[i] != '\0')
+	for (i = 0; i < 10; i++)
 	{
 		_putchar(put[i]);
 		i++;
---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,7 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * Decription: Write a function that prints the alphabet,
+ * in lowercase, followed by a new line.
+ * Return: 0
+ */
