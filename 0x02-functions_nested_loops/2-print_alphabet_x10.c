d5d27cba9529ab05f954b3df7e63e297aa25a4f0
Ayomide Kayode <ayomidekay7@gmail.com>
2023-06-20 15:10:28 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Print alphabet in lowercase 10 times
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z';)
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
