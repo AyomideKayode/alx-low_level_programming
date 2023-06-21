c93b667a2a546efbad2e545af1a54fa11d73b04c
Ayomide Kayode <ayomidekay7@gmail.com>
2023-06-21 14:58:38 -0700

    Leap year

--- 0x03-debugging/3-convert_day.c
+++ 0x03-debugging/3-convert_day.c
@@ -13,40 +13,41 @@ int convert_day(int month, int day)
     switch (month)
     {
         case 2:
-            day = 31 + day;
+            day += 31;
             break;
         case 3:
-            day = 59 + day;
+            day += 59;
             break;
         case 4:
-            day = 90 + day;
+            day += 90;
             break;
         case 5:
-            day = 120 + day;
+            day += 120;
             break;
         case 6:
-            day = 151 + day;
+            day += 151;
             break;
         case 7:
-            day = 181 + day;
+            day += 181;
             break;
         case 8:
-            day = 212 + day;
+            day += 212;
             break;
         case 9:
-            day = 243 + day;
+            day += 243;
             break;
         case 10:
-            day = 273 + day;
+            day += 273;
             break;
         case 11:
-            day = 304 + day;
+            day += 304;
             break;
         case 12:
-            day = 334 + day;
+            day += 334;
             break;
         default:
             break;
     }
-    return (day);
+    return day;
 }
+
