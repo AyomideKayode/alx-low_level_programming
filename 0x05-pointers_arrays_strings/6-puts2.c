5aa5ce7c99e481febcd1a69663c855f39d9673bd
Ayomide Kayode <ayomidekay7@gmail.com>
2023-06-26 08:54:54 -0700

    Winning is only half of it. Having fun is the other half

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * puts2 - prints every other character of a string,
- * starting with the first character, followed by a new line.
  * @str - char array string type
  * Description: prints 1st char, then 3rd, then 5th, etc
  */
@@ -10,10 +9,8 @@ void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0'; i++)
-	{
-		if (i % 2 == 0)
-			_putchar(str[i]);
-	}
+	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
+		_putchar(str[i]);
+
 	_putchar('\n');
 }
