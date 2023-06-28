b2213465283e3f4781bf5e50f6eff0d913b62426
Ayomide Kayode <ayomidekay7@gmail.com>
2023-06-28 01:58:47 -0700

    Code reconfigure for Task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,15 +1,17 @@
 #include "main.h"
 /**
- * puts2 - prints every other character of a string
- * @str: The string to print
- * Description: prints 1st char, then 3rd, then 5th, etc
+ * puts2 - print one char out of 2 of a string
+ * @str: char array string type
+ * Description: Print 1st char, then 3rd, then 5th, etc..
  */
 void puts2(char *str)
 {
 	int i;
 
-	for (i = 0; str[i] != '\0' && i < _strlen(str); i += 2)
-		_putchar(str[i]);
-
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
 	_putchar('\n');
 }
