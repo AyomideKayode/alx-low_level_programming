fefc9446aacc3408b4efe6142d069f1f9a093866
Ayomide Kayode <ayomidekay7@gmail.com>
2023-06-29 06:13:02 -0700

    Expect the best. Prepare for the worst. Capitalize on what comes

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,10 +10,13 @@ char *cap_string(char *str)
 	int caps;
 	char nots[] = ",;.!?(){}\n\t\" ";
 
+	/* iterates over each character in the character array str */
 	for (i = 0, caps = 0; str[i] != '\0'; i++)
 	{
+		/* check for 1st char in str if lowercase */
 		if (str[0] > 96 && str[0] < 123)
 			caps = 1;
+		/* iterates over each char in the nots array */
 		for (c = 0; nots[c] != '\0'; c++)
 		{
 			if (nots[c] == str[i])
@@ -22,13 +25,16 @@ char *cap_string(char *str)
 
 		if (caps)
 		{
+		/* convert to upper by sub 32 ASCII value */
 			if (str[i] > 96 && str[i] < 123)
 			{
 				str[i] -= 32;
 				caps = 0;
 			}
+	/* check if char is upper, if so, leave it. */
 			else if (str[i] > 64 && str[i] < 91)
 				caps = 0;
+	/* check if char is digit, if so, leave it. */
 			else if (str[i] > 47 && str[i] < 58)
 				caps = 0;
 		}
