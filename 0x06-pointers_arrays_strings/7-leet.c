7cecc8fb7809cb29e959fd7e1f327d1747f4b74a
Ayomide Kayode <ayomidekay7@gmail.com>
2023-06-29 07:58:09 -0700

    Changed code for Task 7 to meet with requirements of two loops and one if statement. Compiled alright in terminal.

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -6,40 +6,17 @@
  */
 char *leet(char *s)
 {
-	int i;
+	int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
+	int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
+	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++)
+	for (j = 0; s[j] != '\0'; j++)
 	{
-		while (s[i] == 'a' || s[i] == 'A')
-			s[i] = '4';
-		while (s[i] == 'e' || s[i] == 'E')
-			s[i] = '3';
-		while (s[i] == 'o' || s[i] == 'O')
-			s[i] = '0';
-		while (s[i] == 't' || s[i] == 'T')
-			s[i] = '7';
-		while (s[i] == 'l' || s[i] == 'L')
-			s[i] = '1';
+		for (i = 0; a[i] != '\0'; i++)
+		{
+			if (s[j] == a[i])
+				s[j] = b[i];
+		}
 	}
-
 	return (s);
 }
-
-/**
-  * Descripion - another method with only the for loop
-  * char *leet(char *s)
-  * {
-  * int a[11] = {'a', 'A', 'e', 'E', 'o', 'O', 't', 'T', 'l', 'L'};
-  * int b[11] = {'4', '4', '3', '3', '0', '0', '7', '7', '1', '1'};
-  * int i, j;
-  * for (j = 0; s[j] != '\0'; j++)
-  * {
-  * for (i = 0; a[i] != '\0'; i++)
-  * {
-  * if (s[j] == a[i])
-  * s[j] = b[i];
-  * }
-  * }
-  * return (s);
-  * }
-  */
