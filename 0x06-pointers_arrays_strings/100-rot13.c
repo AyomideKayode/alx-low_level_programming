766752a835cf6bdec16ef0d27f2969fe36f0a88a
Ayomide Kayode <ayomidekay7@gmail.com>
2023-06-29 10:01:48 -0700

    Restructure code for Task 8 ROT-13

--- 0x06-pointers_arrays_strings/100-rot13.c
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -1,23 +1,28 @@
 #include "main.h"
 /**
  * rot13 - rotate characters 13 places in the alphabet
- * @s: string
- * Return: string `s` rotated
+ * @str: string
+ * Return: string `str` rotated
  */
-char *rot13(char *s)
+char *rot13(char *str)
 {
-	int i;
-	char rot_upper[] = "NOPQRSTUVWXYZABCDEFGHIJKLM";
-	char rot_lower[] = "nopqrstuvwxyzabcdefghijklm";
+	int i, j;
 
-	for (i = 0; s[i] != '\0'; i++) /* loop through string */
+	char rot_upper[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char rot_lower[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
 	{
-		/* check if within range of alphabets */
-		if ((s[i] >= 65 && s[i] <= 90) || (s[i] >= 97 && s[i] <= 122))
+		j = 0;
+		while (rot_upper[j] != '\0')
 		{
-			s[i] = rot_upper[s[i] - 65];
-			s[i] = rot_lower[s[i] - 97];
+			if (str[i] == rot_upper[j])
+			{
+				str[i] = rot_lower[j];
+				break;
+			}
+			j++;
 		}
 	}
-	return (s);
+	return (str);
 }
