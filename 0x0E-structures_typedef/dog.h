df0f4e65a3651777bd9574ae97386b24c797c2b6
Ayomide Kayode <ayomidekay7@gmail.com>
2023-07-17 10:17:43 -0700

    New dog. Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,29 +1,24 @@
 #ifndef DOG_H
 #define DOG_H
+
+int _putchar(char c);
 /**
- * struct dog - dog structure
- * @name: name of dog
- * @age: age of the dog
- * @owner: dog owner
+ * struct dog - best friend of humankind
+ * @name: doggo's name
+ * @age: doggo's age
+ * @owner: owner's name
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - Typedef for struct dog
- */
-
-typedef struct dog dog_t;
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
 
-#endif /* struct dog */
-
+#endif /* !DOG_H */
