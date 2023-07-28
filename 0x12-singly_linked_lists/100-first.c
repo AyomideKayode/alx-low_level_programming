e2c0031074d524bd9646edc0ded1955ece98cc8e
AyomideKayode <ayomidekay7@gmail.com>
2023-07-28 09:48:39 +0000

    Task 5 - The Hare and the Tortoise.

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * execute_before_main - executes this function before main function
+ * Note: made possible by prior declaration of "__attribute__ ((constructor))".
+ *
+ * Return: void.
+ */
+
+void __attribute__ ((constructor)) execute_before_main()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
