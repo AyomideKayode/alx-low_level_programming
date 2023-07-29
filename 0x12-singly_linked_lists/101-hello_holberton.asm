afb91ab8177876dfa1d1a92c3f6706ffd847b15e
AyomideKayode <ayomidekay7@gmail.com>
2023-07-29 14:36:24 +0000

    Fixed COde functionality for 101-hello_holberton.

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -3,20 +3,25 @@ extern printf
 
 section .text
 main:
-	push rbp        ; make sure to keep stack aligned
-	mov rbp, rsp
-
 	; Prepare the arguments for printf
-;	xor eax, eax    ; (returning value for printf) clearing eax
-;	lea rdi, [hello_msg]    ; address of message is stored into rdi
+	push rbp		; make sure to keep stack aligned
 	mov rdi, format		; First argument: pointer to the format string
 	mov rsi, hello_msg	; Second argument: pointer to the hello_msg string
+	mov rax, 0
+
+;	mov rbp, rsp
+;	xor eax, eax    ; (returning value for printf) clearing eax
+;	lea rdi, [hello_msg]    ; address of message is stored into rdi
 
 	call printf     ; call the printf function
 
-	add rsp, 16
-	leave           ; mov rsp, rbp, pop rbp
+	pop rbp
+	mov rax, 0
 	ret
+
+;	add rsp, 16
+;	leave           ; mov rsp, rbp, pop rbp
+;	ret
 
 section .data
 hello_msg db "Hello, Holberton", 0
