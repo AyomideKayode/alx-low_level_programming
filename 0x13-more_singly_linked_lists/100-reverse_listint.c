1e0e6c75ed191f265a79d8bf4a84676a138e0379
AyomideKayode <ayomidekay7@gmail.com>
2023-08-01 17:14:09 +0000

    Task 11 - Reverse Linked List.

--- 0x13-more_singly_linked_lists/100-reverse_listint.c
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -8,24 +8,27 @@
 
 listint_t *reverse_listint(listint_t **head)
 {
-	listint_t *prev = NULL; /* Pointer to keep track of previous node */
-	/*listint_t *current = *head; Pointer to traverse the list */
+	listint_t *prev; /* Pointer to keep track of previous node */
+	listint_t *next_node; /* Pointer to kepp track of next node */
 
+	if (head == NULL || *head == NULL)
+		return (NULL);
+
+	/* Traverse through list to reverse the linking */
+	prev = NULL;
 	while (*head != NULL)
 	{
-		listint_t *next_node = (*head)->next; /* Save the next node */
-
+		next_node = (*head)->next; /* Save the next node */
 		/* Reverse the link to the previous node */
 		(*head)->next = prev;
 
 		/* Move both pointers one step forward */
-		prev = *head;
-		*head = next_node;
+		prev = *head; /* prev node updated to be cyrrent node */
+		*head = next_node; /* move current node to next node*/
 	}
 
-/* After the loop, 'prev' will be pointing to the new head (reversed head) */
+	/* After the loop, 'prev' will be pointing to the new head (reversed head) */
 	*head = prev;
 
-	/* Return a pointer to the first node of the reversed list */
-	return (*head);
+	return (*head); /* Return pointer to first node of reversed list */
 }
