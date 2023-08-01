cd51fa5874f5a9287aa4f87b9cea60cf842f6c0d
AyomideKayode <ayomidekay7@gmail.com>
2023-08-01 15:47:54 +0000

    Task 10 - Delete node at index.

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,7 +16,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		return (-1);
 
 	current_node = *head;
-	/* prev_node = NULL;*/
+	prev_node = NULL;
 
 	for (i = 0; i < index && current_node != NULL; i++)
 	{
