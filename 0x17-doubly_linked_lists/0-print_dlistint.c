43842cb8be3b1509ef74f9cafa19d3fa0a3541a1
Ayomide Kay <ayomidekay7@gmail.com>
2023-09-14 14:04:08 +0100

    Task 1: Print List

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+  * print_dlistint - prints a doubly linked list
+  * @h: Head of the list
+  * Return: Number of items in the linked list
+  */
+size_t print_dlistint(const dlistint_t *h)
+{
+  size_t i;
+
+	for (i = 0; h; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
+}
